1fb3b1ccbd1a6f91554db73ea7f41ffdeb5d5377
Abhilash <pakkelaabhilash@gmail.com>
2026-06-20 12:25:52 +0530

    Sat

--- feature/presentation/src/commonMain/kotlin/com/smach/zapmancer/features/projects/state/ProjectListUiState.kt
+++ feature/presentation/src/commonMain/kotlin/com/smach/zapmancer/features/projects/state/ProjectListUiState.kt
@@ -1,25 +1,3 @@
 package com.smach.zapmancer.features.projects.state
 
-import com.smach.zapmancer.domain.model.Article
 
-data class ProjectListUiState(
-    val id: Int = 0,
-    val isLoading: Boolean = false,
-    val isRefreshing: Boolean = false,
-    val isPaging: Boolean = false,
-    val trendingArticles: List<Article> = emptyList(),
-    val recommendedArticles: List<Article> = emptyList(),
-    val categories: List<String> = listOf(
-        "For You",
-        "Following",
-        "Technology",
-        "Design",
-        "Writing",
-        "Programming",
-    ),
-    val selectedCategory: String = "For You",
-    val page: Int = 1,
-    val endReached: Boolean = false,
-    val error: String? = null,
-    val title: String = "Home",
-)
