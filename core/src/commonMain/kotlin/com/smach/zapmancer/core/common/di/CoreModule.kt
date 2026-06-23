39cb314d63a8ec64742fdbd2e497a34d34a78d83
Abhilash <pakkelaabhilash@gmail.com>
2026-06-23 10:55:14 +0530

    datastore

--- core/src/commonMain/kotlin/com/smach/zapmancer/core/common/di/CoreModule.kt
+++ core/src/commonMain/kotlin/com/smach/zapmancer/core/common/di/CoreModule.kt
@@ -2,9 +2,6 @@ package com.smach.zapmancer.core.common.di
 
 import com.smach.zapmancer.core.common.utils.DataStoreStorage
 import com.smach.zapmancer.core.common.utils.createDataStore
-import com.smach.zapmancer.core.database.db.AppDatabase
-import com.smach.zapmancer.core.database.db.getDatabaseBuilder
-import com.smach.zapmancer.core.database.db.getRoomDatabase
 import com.smach.zapmancer.core.monitoring.AnalyticsService
 import com.smach.zapmancer.core.monitoring.NapierAnalyticsService
 import com.smach.zapmancer.core.network.ktor.provideHttpClient
@@ -15,15 +12,6 @@ val coreModule =
     module {
         single { createDataStore() }
         single { DataStoreStorage(get()) }
-
-        single {
-            getDatabaseBuilder()
-        }
-        single<AppDatabase> {
-            getRoomDatabase(get())
-        }
-        single { get<AppDatabase>().articleDao() }
-        single { get<AppDatabase>().recentSearchDao() }
 
         single<AnalyticsService> { NapierAnalyticsService() }
 
--- core/src/commonMain/kotlin/com/smach/zapmancer/core/common/utils/DataStoreBuilder.kt
+++ core/src/commonMain/kotlin/com/smach/zapmancer/core/common/utils/DataStoreBuilder.kt
@@ -3,6 +3,8 @@ package com.smach.zapmancer.core.common.utils
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 
+// this project is currently on the datastore and migrate to sqldelight now the all the DataStoreStorage file has saveString and getString functions these are needed to migrate to sqldelight
+
 expect fun createDataStore(): DataStore<Preferences>
 
 internal const val DATA_STORE_FILE_NAME = "scriptside.preferences_pb"
