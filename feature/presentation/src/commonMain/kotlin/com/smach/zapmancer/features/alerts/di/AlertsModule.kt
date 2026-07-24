d7be7882ed0a66034ec5570af2fae1a8660e8f59
Abhilash <pakkelaabhilash@gmail.com>
2026-07-24 16:21:17 +0530

    ..

--- feature/presentation/src/commonMain/kotlin/com/smach/zapmancer/features/alerts/di/AlertsModule.kt
+++
@@ -1,9 +0,0 @@
-package com.smach.zapmancer.features.alerts.di
-
-import com.smach.zapmancer.features.alerts.viewmodel.NotificationViewModel
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
-
-val alertsModule = module {
-    viewModelOf(::NotificationViewModel)
-}
--- feature/presentation/src/commonMain/kotlin/com/smach/zapmancer/features/settings/di/SettingsModule.kt
+++
@@ -1,9 +0,0 @@
-package com.smach.zapmancer.features.settings.di
-
-import com.smach.zapmancer.features.settings.viewmodel.SettingsViewModel
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
-
-val settingsModule = module {
-    viewModelOf(::SettingsViewModel)
-}
